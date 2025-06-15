72e7c6158606585c300db5a0aae8609a180d2e78
tjp1010 <tjpdev@gmail.com>
2025-06-15 17:44:00 +0100

    Added Button no Functionality

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,9 +1,23 @@
 import styles from "./todoItems.module.css";
 
 export default function TodoItems({ item }) {
+  function handleDelete(item) {
+    console.log("delete button clicked for item", item);
+  }
   return (
     <div className={styles.item}>
-      <div className={styles.itemName}>{item}</div>
+      <div className={styles.itemName}>
+        {item}
+        <span>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteBtn}
+          >
+            x
+          </button>
+        </span>
+      </div>
+
       <hr className={styles.line} />
     </div>
   );
